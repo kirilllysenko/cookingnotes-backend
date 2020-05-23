2499571ca5181357000bd9df2bcd88338bd9e16a
Kirill Lysenko <kirill.lysenko.msk@gmail.com>
2020-05-23 23:02:05 +0300

    Added all files(maybe)

---
+++ src/test/java/com/kirill/cookingnotes/CookingnotesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kirill.cookingnotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookingnotesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
